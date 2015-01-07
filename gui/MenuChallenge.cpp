a23ab7de80b9adcf588fa7b7579edc57bbd21ec1
sofiane <polaris.mov@gmail.com>
2015-01-08 00:59:34 +0100

    update challenge

--- gui/MenuChallenge.cpp
+++ gui/MenuChallenge.cpp
@@ -8,6 +8,7 @@
 
 #include "../GameState.hpp"
 #include "../DataManager.hpp"
+#include "../Challenge.hpp"
 
 gui::MenuChallenge::MenuChallenge()
 {
@@ -24,7 +25,7 @@ gui::MenuChallenge::~MenuChallenge()
 
 void gui::MenuChallenge::update()
 {
-
+	
 	if( egn::Keyboard::isActive( "up" ) )
 	{
 		updateCursor( -1 );
@@ -35,31 +36,58 @@ void gui::MenuChallenge::update()
 		updateCursor( 1 );
 		egn::Keyboard::setActive( "down", false );
 	}
-	if ( egn::Keyboard::isActive("return"))
-    {
-        switch (m_Cursor)
-        {
-            case 0:
-            GameState::set( Gamestate::MENU_START );
-            break;
-        }
-        egn::Keyboard::setActive( "return", false );
-    }
+
+	if ( egn::keyboard::isActive( "left") )
+	{
+		updateCursorHorizontale( -1 );
+		egn::keyboard::setActive( "left", false );
+	}
+	else if ( egn::keyboard::isActive( "right") )
+	{
+		updateCursorHorizontale( 1 );
+		egn::keyboard::setActive( "right", false );
+	}
+
 }
+Challenge challenge = DataManager::get()->basic_get_challenge_list().at(m_CursorH);
 
 void gui::MenuChallenge::draw( egn::Window& window )
 {
+	int score = 0;
+
+	Challenge challenge = DataManager::get()->basic_get_challenge_list().at(m_CursorH);
+
 	window.drawRect( egn::FloatRect( 0, 0, 800, 600 ), egn::Color( 0, 0, 0, 180 ) );
 	egn::FontManager::get()->write( window, "shoes", "Challenge", egn::Vector2f( 255, 60 ), 50, egn::Color::Black );
 
-	if( m_Cursor == 0 )
+	if ( egn::Keyboard::isActive("return"))
 	{
-		egn::FontManager::get()->write(window, "shoes","Retour",egn::Vector2f(330, 550 ), 25, egn::Color::Yellow);
+			switch( challenge.getType() )
+		{	
+			case ChallengeType::SCORE_TIME:
+			score = challenge.getScore()
+			break;
+
+			case 0:
+            GameState::set( Gamestate::MENU_START );
+            break;
+
+		}
+		egn::Keyboard::setActive( "return", false );
 	}
-	else 
-	{
-		egn::FontManager::get()->write(window, "shoes","Retour",egn::Vector2f(330, 550 ), 25, egn::Color::Black);	
-	}
+
+	m_Button[0].setPosition( egn::Vector2f( 250, 430 ) );
+    m_Button[1].setPosition( egn::Vector2f( 250, 430 ) );
+    if( m_Cursor == 0 )
+    {
+        window.draw( m_Button[1] );
+    }
+    else 
+    {
+        window.draw( m_Button[0] );
+    }
+    egn::FontManager::get()->write( window, "shoes", "Retour", egn::Vector2f( 340, 432 ), 30, egn::Color::Black );
+
 }
 
 void gui::MenuChallenge::updateCursor( int value )
@@ -67,4 +95,11 @@ void gui::MenuChallenge::updateCursor( int value )
 	m_Cursor += value;
 	if( m_Cursor < 0 ) m_Cursor = 1;
 	if( m_Cursor > 1 ) m_Cursor = 0;
+}
+
+void gui::MenuChallenge::updateCursorHorizontal( int value )
+{
+	m_CursorH += value;
+	if( m_CursorH < 0 ) m_CursorH = DataManager::get()->basic_get_challenge_list().size() - 1;
+	if( m_CursorH > DataManager::get()->basic_get_challenge_list().size() - 1 ) m_CursorH = 0;
 }
--- gui/MenuChallenge.hpp
+++ gui/MenuChallenge.hpp
@@ -1,6 +1,8 @@
 #ifndef GUI_MENUCHALLENGE_HPP
 #define GUI_MENUCHALLENGE_HPP
 
+#include "../DataManager.hpp"
+
 #include "../egn/Window.hpp"
 
 #include "GuiBase.hpp"
@@ -18,12 +20,18 @@ namespace gui
 		
 		virtual void draw( egn::Window& window );
 
+		virtual void set( const std::string& var, int value );
+        virtual void set( const std::string& var, std::string value );
+
 		private:
 		void updateCursor( int value );
-	private:
+		void updateCursorHorizontal( int value);
+	    
+	    private:
 		egn::Sprite m_Button[2];
 
 		int m_Cursor;
+		int m_CursorH;
 	};
 	
 }
